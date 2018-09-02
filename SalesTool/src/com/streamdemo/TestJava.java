541e5ef64c5ff49102d2b184808eb9f3b2f19502
manishsharma23 <manish.sharma23@gmail.com>
2018-09-02 22:45:52 +0530

    Demo

---
+++ SalesTool/src/com/streamdemo/TestJava.java
@@ -0,0 +1,10 @@
+package com.streamdemo;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adfsadf asf sa fasf fas   ");
+	}
+
+}

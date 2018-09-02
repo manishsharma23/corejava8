f3571ea87a02775e791649ea0e5dcd80143094b6
manishsharma23 <manish.sharma23@gmail.com>
2018-09-02 23:04:14 +0530

    First change

--- SalesTool/src/com/streamdemo/TestJava.java
+++ SalesTool/src/com/streamdemo/TestJava.java
@@ -5,6 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Adfsadf asf sa fasf fas   ");
+		System.out.println("push 2 step..");
 	}
 
 }

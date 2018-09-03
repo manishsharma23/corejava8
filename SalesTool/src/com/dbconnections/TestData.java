117427d163da138e68c66da1da8361d9b6794a26
manishsharma23 <manish.sharma23@gmail.com>
2018-09-03 22:11:41 +0530

    testdata

---
+++ SalesTool/src/com/dbconnections/TestData.java
@@ -0,0 +1,12 @@
+package com.dbconnections;
+
+public class TestData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("testData..............>>>");
+
+	}
+
+}

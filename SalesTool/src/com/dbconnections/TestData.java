95af487ab90b2f944685b3e2ba8fe2f747a4c714
manishsharma23 <manish.sharma23@gmail.com>
2018-09-03 22:17:48 +0530

    Prod

--- SalesTool/src/com/dbconnections/TestData.java
+++ SalesTool/src/com/dbconnections/TestData.java
@@ -5,7 +5,7 @@ public class TestData {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("testData..............>>>");
+		System.out.println("testData.......manish.......>>>");
 
 	}
 
